83cd0f2abbb788e6c2d09358df4eff827b968166
sonika-tang <sonikatang630@gmail.com>
2026-02-06 09:57:23 +0700

    BLA-001 Rides Service

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,3 +1,5 @@
+import 'package:blabla/model/ride/locations.dart';
+
 import '../data/dummy_data.dart';
 import '../model/ride/ride.dart';
 
@@ -8,5 +10,16 @@ import '../model/ride/ride.dart';
 class RidesService {
   static List<Ride> allRides = fakeRides;
 
+  static List<Ride> _filterByDeparture(Location departure) {
+    return allRides
+        .where((ride) => ride.departureLocation == departure)
+        .toList();
+  }
 
+  static List<Ride> _filterBySeatRequested(int seatRequested) {
+    return 
+  }
+
+  static List<Ride> _filterBy({Location? departure, int? seatRequestd}) {
+  }
 }
